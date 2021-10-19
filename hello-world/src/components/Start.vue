cec5aa3e445d1e164fe752a4920e636a8a00852f
harveyj123 <71397990+harveyj123@users.noreply.github.com>
2021-10-18 23:19:29 -0400

    as

--- hello-world/src/components/Start.vue
+++ hello-world/src/components/Start.vue
@@ -1,14 +1,24 @@
 <template>
+ <section class="img-select-section">
   <header class="home">
     <h1> {{title}} </h1>
-    <div >
+      </header>
+      <div class="img-gallery-container-outer">
+    <div class="img-gallery-container">
+      <div class="img-gallery-container-inner">
+      <img :src="image" class="img" >
+      </div>
+    </div>
+    <div class="img-gallery-container">
         <ul class="img-gallery">
-          <li v-for="image in images" v-bind:key="image">
-            <img :src="image">
+          <li v-for="image in images" v-bind:key="image" class="list">
+            <img :src="image.variantImage" class="img">
           </li>
         </ul>
     </div>
-  </header>
+    </div>
+       </section>
+
   </template>
 
     
@@ -21,19 +31,87 @@ export default {
     return {
       image : "https://www.personality-database.com/profile_images/349339.png",
       title: ":3 asdasd",
-      images: ["https://www.personality-database.com/profile_images/349339.png", "https://www.personality-database.com/profile_images/349339.png","https://www.personality-database.com/profile_images/349339.png"]
+      images: [ {
+        variantId: 1,
+        variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'
+      },
+       {
+        variantId: 2,
+        variantImage: 'https://www.personality-database.com/profile_images/349339.png'
+      },
+       {
+        variantId: 3,
+        variantImage: 'https://www.personality-database.com/profile_images/349339.png'
+      },
+       {
+        variantId: 3,
+        variantImage: 'https://www.personality-database.com/profile_images/349339.png'
+      },
+       {
+        variantId: 4,
+        variantImage: 'https://www.personality-database.com/profile_images/349339.png'
+      },
+       {
+        variantId: 5,
+        variantImage: 'https://www.anime-planet.com/images/characters/34170.jpg?t=1529534556'
+      }, ]
     };
   },
   methods: {
 
-  },
-};
+}}
 </script>
 
 
 <style>
+  
+html,
+body,
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+.img-select-section {
+  height: 100vh;
+  width: 100vw;
+}
+.img-gallery-container-outer{
+display: flex;
+flex-direction: row;
+
+}
+
+.img-gallery-container-inner {
+  height: 80%;
+  width: 80%;
+}
+
+.img-gallery-container {
+  display: flex;
+    justify-content: center;
+  align-content: center;
+margin: 2rem;
+height: 60%;
+width: 50%;
+}
+
 .img-gallery {
+    display: flex;
+    justify-content: center;
+    align-content: center;
+   flex-wrap: wrap;
   list-style-type: none;
+}
+
+.list {
+  padding: .5rem;
+  margin: .5rem;
+}
+
+.img {
+  width: 100%;
+  height: auto;
 }
 </style>
 
