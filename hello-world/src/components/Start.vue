f71a00792fa1c7639454dc52de60af66a884e7f6
harveyj123 <71397990+harveyj123@users.noreply.github.com>
2021-10-31 21:37:35 -0400

    yep

--- hello-world/src/components/Start.vue
+++ hello-world/src/components/Start.vue
@@ -1,69 +1,68 @@
 <template>
- <section class="img-select-section">
-  <header class="home">
-    <h1> {{title}} </h1>
-      </header>
-      <div class="img-gallery-container-outer">
-    <div class="img-gallery-container">
-      <div class="img-cont">
-      <img :src="image" id="img" v-bind:style="{ opacity:  currentValue/100 , borderRadius: borderRadius + 'rem',
-      border: 'borderThickness' + borderVal}">
-      </div>
-</div>
-    <div class="img-gallery-container" >
+
+  <section class="img-select-section">
+    
+    <header class="home">
+       <h1> {{title}} </h1>
+    </header>
+
+    <div class="img-gallery-container-outer">
+      <div class="img-gallery-container">
+        <figure class="img-cont">
+        <img :src="image" id="img" v-bind:style="{ opacity:  opvalue , borderRadius: borderRadius + 'rem',
+        border: borderVal, filter: 'blur'+'('+blurValue+'px'+')'}">
+      </figure>
+    </div>
+
+    <figure class="img-gallery-container" >
         <ul class="img-gallery">
           <li v-for="image in images" v-bind:key="image" class="list">
             <img @click="updateImg(image.variantImage)" :src="image.variantImage" class="img" id="listImg">
           </li>
         </ul>
+    </figure>
+
     </div>
-    </div>
+
     <div class="img-editer-container">
- <div class="slidecontainer">
-        <input
-          ref="input"
-          v-model="borderRadius"
-          type="range"
-          :min="0"
-          :max="15"
-          class="slider"
-          @input="onInput"
-        >
-        
-          </div>
-            <label for="input">border radius</label>
-      <div class="slidecontainer">
-        <input
-          ref="input"
-          v-model="currentValue"
-          type="range"
-          :min="5"
-          :max="100"
-          class="slider"
-          @input="onInput"
-        >
+     
+     <div class="slidecontainer">
+        <input type="range" min="0" max="15" v-model="borderRadius" id="borderRadiusSlider" class="slider">  
+         <label for="borderRadiusSlider" class="sliderLabel">border radius</label> 
       </div>
- <label for="input">opacity</label>
-<div>
-<input type="radio" id="border-val-1" value="solid" v-model="borderVal">
-<label for="border-val-1">solid</label>
-<input type="radio" id="border-val-2" value="dashed" v-model="borderVal">
-<label for="border-val-2">dashed</label>
-<input type="radio" id="border-val-3" value="thick double" v-model="borderVal">
-<label for="border-val-3">thick double</label>
-<input type="radio" id="border-val-4" value="ridge" v-model="borderVal">
-<label for="border-val-4">ridge</label>
-<input type="radio" id="border-val-4" value="none" v-model="borderVal">
-<label for="border-val-4">no border</label>
-</div>
 
-<input id="border-thickness-val" v-model="borderThickness" placeholder="units in pixels">
-<label for="border-thickness-val">input a numerical val for border thickness</label>
+           <div class="slidecontainer">
+        <input type="range" min="0" max="15" v-model="blurValue" id="blurSlider" class="slider">  
+         <label for="blurSlider" class="sliderLabel">img blur</label> 
+      </div>
+
+    <div class="slidecontainer">
+        <input type="range" min="0.05" max="1" step="0.01" v-model="opvalue" id="opacitySlider" class="slider"> 
+        <label for="opacitySlider" class="sliderLabel">slide the slider down to decrease opacity</label>
+      </div>
+    <div class="border-container">
+
+        <input type="radio" id="border-val-1" value=".5rem solid" v-model="borderVal" class="radio-style">
+        <label for="border-val-1" class="radio-style">solid</label>
+
+        <input type="radio" id="border-val-2" value=".3rem dashed" v-model="borderVal" class="radio-style">
+        <label for="border-val-2" class="radio-style">dashed</label>
+
+        <input type="radio" id="border-val-3" value="thick double" v-model="borderVal" class="radio-style">
+        <label for="border-val-3" class="radio-style">thick double</label>
+
+        <input type="radio" id="border-val-4" value="none" v-model="borderVal" class="radio-style">
+        <label for="border-val-4" class="radio-style">no border</label>
     </div>
 
-       </section>
+    </div>
+    
 
-  </template>
+
+
+  </section>
+
+</template>
 
 
 
@@ -71,35 +70,23 @@
 export default {
   name: "Home",
   components: {},
-  props: {
-    value: {
-      type: Number,
-      required: true
-    },
-    min: {
-      type: Number,
-      required: true
-    },
-    max: {
-      type: Number,
-      required: true
-    }
-  },
+
   data() {
     return {
-      currentValue: this.value,
-       borderRadius: "",
-       borderVal: "",
-      opvalue : "",
-      image : "https://www.personality-database.com/profile_images/349339.png",
-      title: "Image Changer :3",
+ 
+      borderRadius: "0",
+      blurValue: "0",
+      borderVal: "",
+      opvalue : "1",
+      image : "https://i.pinimg.com/736x/53/03/86/53038646a6615d11ca2ffb6c47d4878c.jpg",
+      title: "Image Changer",
       images: [ {
         variantId: 1,
         variantImage: 'https://i.pinimg.com/originals/10/46/ce/1046ce096a18aeac4074dfc23ee96958.jpg'
       },
        {
         variantId: 2,
-        variantImage: 'https://www.personality-database.com/profile_images/349339.png'
+        variantImage: 'https://i.pinimg.com/736x/53/03/86/53038646a6615d11ca2ffb6c47d4878c.jpg'
       },
        {
         variantId: 3,
@@ -123,10 +110,6 @@ export default {
     updateImg(variantImage) {
       this.image = variantImage
     },
-
-      onInput() {
-      this.$emit('input', parseInt(this.currentValue));
-    }
 }}
 </script>
 
@@ -179,7 +162,7 @@ width: 50%;
   list-style-type: none;
 }
 .list {  
-  margin: .3rem;
+
     height: 30%;
   width: 45%;
 
@@ -190,24 +173,23 @@ width: 50%;
 }
 .img {
   border-radius: .3rem;
-    width: 100%;
-     height: 100%;
+    width: 95%;
+     height: 95%;
 }
 .img-cont {
-    justify-content: center;
+  justify-content: center;
   align-items: center;
-display: flex;
+  display: flex;
   height: 90%;
   width: 95%;
 }
 #img {
-
-     max-width: 85%;
+    max-width: 85%;
     height: 85%;
 }
 
 .change-img-container {
-    text-align: center;
+  text-align: center;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -216,16 +198,9 @@ display: flex;
 }
 
 
-.slider {
-	width: 65%;
-	height: .5rem;
-	border-radius: 2px;
-	background: lightsalmon;
-}
-
 
 .slidecontainer {
-  margin: 2rem;
+  height: 20%;
   display: flex;
   justify-content: center;
     flex-direction: column;
@@ -239,6 +214,25 @@ display: flex;
   justify-content: center;
   align-items: center;
 height: 40%;
-width: 80%;
+width: 100%;
+}
+
+.slider {
+  width: 60%;
+}
+
+.sliderLabel {
+  margin: .5rem;
+}
+
+.border-container {
+  width: 100%;
+  height: 20%;
+    display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.radio-style {
+  margin: 1rem;
 }
 </style>
